32db2bea273a0950ac1901051c5c898637c87219
dwd-umd <ddarg@umd.edu>
2019-04-26 03:07:14 -0400

    Add .venv-checking safety to _freeze_requirements.sh

--- _freeze_requirements.sh
+++ _freeze_requirements.sh
@@ -1,3 +1,16 @@
 #! /bin/bash
 
-pip freeze > requirements.txt
+# Check you have an activated venv and you're in the right directory for it
+if [[ $VIRTUAL_ENV == $PWD'/.venv' ]]; then
+    echo "Freezing requirements in present virtual environment"
+    pip freeze >requirements.txt
+else
+    echo "
+    ---------------------------------------------------------------------------------
+        You need to have THIS directory's .venv activated to freeze requirements!
+
+        You are now in:    $PWD
+        Your venv is from: $([ -z ${VIRTUAL_ENV+x} ] && echo "NONE" || echo "$VIRTUAL_ENV")
+    ---------------------------------------------------------------------------------
+    "
+fi
