0ba0ec041c01a24021cb098f5b5bd50b31a2ae26
dwd-umd <ddarg@umd.edu>
2019-04-26 12:44:49 -0400

    Test precommit hook v2

--- _precommit_hook.sh
+++ _precommit_hook.sh
@@ -8,12 +8,12 @@ echo '''
 
 '''
 
-if [ ! -z ${DONT_FORMAT_ON_CODE_COMMIT+x} ]; then
-    # Auto-format all python scripts
-    .venv/bin/autopep8 -ir src/**
-    .venv/bin/autopep8 -ir tests/**
-else
+if [ -z ${DONT_FORMAT_ON_CODE_COMMIT+x} ]; then
     echo """
         Skipping auto-format!!!
     """
+else
+    # Auto-format all python scripts
+    .venv/bin/autopep8 -ir src/**
+    .venv/bin/autopep8 -ir tests/**
 fi
